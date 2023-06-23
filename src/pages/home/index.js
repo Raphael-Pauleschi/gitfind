16c4bb76f582ace84ececc7f75484823419da6fc
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-06-23 17:33:03 -0300

    header and image background

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,18 @@
+import { Header } from "../../components/Header";
+import background from '../../assets/background.png'
+
 function App() {
-  return <div className="App">ai</div>;
+  return( 
+
+   <div className="App">
+  <Header/>
+  <div className="conteudo">
+    <img src={background} className="background" alt="Git symbol"/>
+  </div>
+  </div>
+
+ 
+  );
 }
 
 export default App;
